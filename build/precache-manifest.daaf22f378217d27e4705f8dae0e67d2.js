9e46e3408a15ebd5517854a26d6f78242e35b89c
nandinirai <rainandini4@gmail.com>
2019-01-29 15:34:00 +0530

    build commit

---
+++ build/precache-manifest.daaf22f378217d27e4705f8dae0e67d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/static/media/bg (1).b6796c73.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "616156f322c5640565d4",
+    "url": "/static/js/main.616156f3.chunk.js"
+  },
+  {
+    "revision": "f1a414959f7fe88399a4",
+    "url": "/static/js/1.f1a41495.chunk.js"
+  },
+  {
+    "revision": "616156f322c5640565d4",
+    "url": "/static/css/main.ce8d137f.chunk.css"
+  },
+  {
+    "revision": "f1a414959f7fe88399a4",
+    "url": "/static/css/1.8ed00045.chunk.css"
+  },
+  {
+    "revision": "c2bb266a9e58a505ad8a17c0bc0eaf07",
+    "url": "/index.html"
+  }
+];
